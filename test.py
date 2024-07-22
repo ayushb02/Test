b02ef01bc7c7fc2f053875292ff48b54bb524d11
ayushb02 <141908430+ayushb02@users.noreply.github.com>
2024-07-22 11:14:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
 
+class BankAccount:
+    def __init__(self, name, balance=0.0):
+        self.name = name
+        self.balance = balance
+      
+ # This fuction will update the account in the event of deposit
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+      
+  # This fuction will update the account in the event of withdrawal
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print('Balance is not sufficient.')
+        else:
+            self.balance -= amount
+        return self.balance
+ 
+
+account = BankAccount('John Doe', 100.5)
+print(account.deposit(30))
+print(account.withdraw(50))
+print(account.withdraw(100))
